b08991d8059c61519251b5605f84db8364cf2264
Milan Sipos <Sipos.Milan22@gmail.com>
2025-11-17 23:44:00 +0100

    cleanup:updated app entry point from uikit to swiftui

---
+++ ios-app/LightAR/PlacementSettings.swift
@@ -0,0 +1,24 @@
+
+import SwiftUI
+import RealityKit
+import Combine
+
+class PlacementSettings {
+    
+    // if user selects model in BrowseView, this property is set
+    @Published var selectedModel: TextureModel? {
+        willSet(newValue) {
+            print("Setting selectedModel to \(String(describing: newValue?.name))")
+        }
+    }
+    // if user confirms in PlacementView, the value of selectedModel is assigned to Confirmedmodel
+    @Published var confirmedModel: TextureModel? {
+        willSet(newValue) {
+            guard let model = newValue else {
+                print("Clearing confirmedModel")
+                return
+            }
+            print("Setting confirmedModel to \(model.name)")
+        }
+    }
+}
